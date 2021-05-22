49983cd024c831e7dba99eef1056ea1099a98b0f
ware255 <74850252+ware255@users.noreply.github.com>
2021-05-22 13:06:48 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 
 int main(int argc; char** argv){
     if (!argc){
-        for (i;i <= 0;i++){
-            if (i == 0) {
-                hata1:
-                printf("Hello, World!\n");
-                getchar();
-                exit(0);
+        for (int i;i <= 0;++i){
+            while (true) {
+                if (i == 0) {
+                    hata1:
+                    printf("Hello, World!\n");
+                    getchar();
+                    exit(0);
+                }
             }
         }
     } else {
