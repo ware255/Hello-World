f8e6e407f2e34910ae9f858efe08df79f7e947e9
われ <74850252+ware255@users.noreply.github.com>
2022-04-25 17:38:26 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,25 @@
+/*
+ * このソースコードは難読化目的で作成しました。
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc; char** argv){
-    if (!argc){
-        for (int i = 0;i < 1;i++){
-            while (true) {
+int main(int argc, char* argv[]) {
+    const char *A = "Hello, World!\n";
+    hata1:
+    if (argc < 1) {
+        for (int i = 0; i < 1; i++) {
+            while (1) {
                 if (i == 0) {
-                    hata1:
-                    printf("Hello, World!\n");
-                    getchar();
+                    printf("%s", A);
                 }
                 break;
             }
             break;
         }
     } else {
-        hata1;
+        argc = 0;
+        goto hata1;
     }
     return 0;
 }
