a6b7593abc15aa632427c61dfd8dd69ccb50de84
われ <74850252+ware255@users.noreply.github.com>
2022-07-09 13:48:55 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,20 +3,25 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-const char* hello() {
-    const char *A = "Hello, World!\n";
-    return A;
+struct H {
+    char hentai[50];
+};
+
+void hello(char str[]) {
+    strcpy(str, "Hello, World!");
 }
 
 int main(int argc, char* argv[]) {
+    struct H sex;
     hata1:
     if (argc < 1) {
         for (int i = 0; i < 1; i++) {
             while (1) {
-                if (i == 0) {
-                    printf("%s", hello());
-                }
+                hello(sex.hentai);
+                if (i == 0) printf("%s\n", sex.hentai);
+                else printf("%s\n", sex.hentai);
                 break;
             }
             break;
