febbbac6a2622550849d529f89ac645eb1603966
われ <74850252+ware255@users.noreply.github.com>
2022-07-23 17:05:24 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,27 @@
 /*
  * このソースコードは難読化目的で作成しました。
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #define HW "Hello, World!"
 
-  struct/*                  */_{char
-  /**/___[                  50];};/*
-  */void/*                  */h(char
-  /**/str[                  ]){/*acs
-  */strcpy                  (str, HW
-  );}int/*                  zs*/main
-  (int/**/argc,char*argv[]){struct/*
-  */_/**/__;hata1:if(argc<1){for(int
-  /**/i=0;i<1;i++){while(1){h(__.___
-  );if(i==                  0)printf
-  ("%s\n",                  __.___);
-  else/**/                  printf/*
-  */(/*s*/                  "%s\n"/*
-  */,__./*                  */___)/*
-  */;break                  ;}break;
-
-
-}}else{argc=0;goto/**/hata1;}return/**/0;}
+    struct/*                  */_{char
+    /**/___[                  50];};/*
+    */void/*                  */h(char
+    /**/str[                  ]){/*acs
+    */strcpy                  (str, HW
+    );}int/*                  zs*/main
+    (int/**/                  argc,/*d
+    */char/*                  */*argv[
+    ]){struct/**/_/**/__;hata1:if(argc
+    <1){for(int/**/i=0;i<1;i++){while(
+    1){h(__.___);if(i==0)printf("%s\n",
+    __.___);                  else/**/
+    printf/*                  */(/*s*/
+    "%s\n"/*                  */,__./*
+    */___)/*                  */;break
+    ;}break;                  };;}else
+    {argc=0;                  goto/**/
+    hata1;};                  return/*
+    */(0/*+=                  0x0*/);}
