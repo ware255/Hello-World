d52b38cc13ddff849737634127bccaaabde058e9
われ <74850252+ware255@users.noreply.github.com>
2022-07-23 16:38:04 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,31 +4,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define HW "Hello, World!"
 
-struct H {
-    char hentai[50];
-};
+  struct/*                  */_{char
+  /**/___[                  50];};/*
+  */void/*                  */h(char
+  /**/str[                  ]){/*acs
+  */strcpy                  (str, HW
+  );}int/*                  zs*/main
+  (int/**/argc,char*argv[]){struct/*
+  */_/**/__;hata1:if(argc<1){for(int
+  /**/i=0;i<1;i++){while(1){h(__.___
+  );if(i==                  0)printf
+  ("%s\n",                  __.___);
+  else/**/                  printf/*
+  */(/*s*/                  "%s\n"/*
+  */,__./*                  */___)/*
+  */;break                  ;}break;
 
-void hello(char str[]) {
-    strcpy(str, "Hello, World!");
-}
 
-int main(int argc, char* argv[]) {
-    struct H sex;
-    hata1:
-    if (argc < 1) {
-        for (int i = 0; i < 1; i++) {
-            while (1) {
-                hello(sex.hentai);
-                if (i == 0) printf("%s\n", sex.hentai);
-                else printf("%s\n", sex.hentai);
-                break;
-            }
-            break;
-        }
-    } else {
-        argc = 0;
-        goto hata1;
-    }
-    return 0;
-}
+}}else{argc=0;goto/**/hata1;}return/**/0;}
