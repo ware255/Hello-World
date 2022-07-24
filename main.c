4ce9b817d7dbda6a893a8bbffc5ad7bff11d4a15
われ <74850252+ware255@users.noreply.github.com>
2022-07-24 22:16:33 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,36 @@
 /*
  * このソースコードは難読化目的で作成しました。
+ * 愛はありません。iだけに。
  */
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#define HW "Hello, World!"
-
-    struct/*                  */_{char
-    /**/___[                  50];};/*
-    */void/*                  */h(char
-    /**/str[                  ]){/*acs
-    */strcpy                  (str,HW)
-    ;}int/*z                  s*/main(
-    int/*(*/                  argc,/*d
-    */char/*                  */*argv[
-    ]){struct/**/_/**/__;hata1:if(argc
-    <1){for(int/**/i=0;i<1;i++){while(
-    1){h(__.___);if(i==0)printf("%s\n"
-    ,__.___)                  ;else/*{
-    */printf                  /**/(/*s
-    */"%s\n"                  ,__./*__
-    */___)/*                  */;break
-    ;}break;                  };;}else
-    {argc=0;                  goto/**/
-    hata1;};                  return/*
-    */(0/*+=                  0x0*/);}
+                char
+            (HW)[]="Hwz\
+          ecl(ldox,f\x20jW\
+         owrelfda!ipn1qbgn4\
+          *a;";struct/*ad*/
+		    H/*_*/{char(
+                rast
+                
+         )[50];/*vkscsna*/};
+         #include<stdio.h>//
+         void(hello)(char(//
+         str)[/**/]){int(j)=
+         {(int)0};for(int(i)
+         ={(int)0};i<45;i+=2
+         ){str[j]=HW[i];if((
+         HW)[i]=='H'){i=11;}
+         else/**/if(HW[i]==\
+         'W'){i+=8;};j++;};}
+         int(main)(int(argc)
+         ,char*argv[])/*}*/{
+         struct/**/H/**/sex;
+         hata1:if(argc<(1)){
+         for(int(i)=0;i<(1);
+         i++){while(1){hello
+         (sex.rast);if(i==0)
+         {printf("%s\n",/**/
+         sex.rast);}else{/*,
+         /"}*/printf("%s\n",
+         sex.rast);}break;};
+         break;}}else{argc=(
+         int)0;goto/**/hata1
+         ;}return((9-1)%2);}
